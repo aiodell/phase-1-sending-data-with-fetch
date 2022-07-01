76bcdf25efdb2aa01f30b44d66b0f8e055cc46e7
Andrianna <andrianna.odell@gmail.com>
2022-07-01 11:55:09 -0400

    commit tree 1d75b288df56552a8bedd038dbff255a23d99bad

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+let name;
+let email;
+
+function submitData(name, email){
+
+    let reqBody = {
+        name: name,
+        email : email,
+    }
+
+    const Object = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" :  "application/json",
+        },
+        Body: JSON.stringify({
+            // data goes here
+            name : reqBody.name,
+            email: reqBody.email
+        }),
+    }
+
+    return fetch("http://localhost:3000/users", Object)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object);
+        })
+        .catch(function (error) {
+            alert("Unauthorized Access: code 401");
+            console.log(error.message);
+        });
+}
+
+
+
